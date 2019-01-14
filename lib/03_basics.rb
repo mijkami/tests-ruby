96c9422fede8e22aedbe3e80b3c236e1c41ca870
mijkami <dimitri.gaillot@gmail.com>
2019-01-14 14:25:45 +0100

    ajout de lib/03_basics.rb

---
+++ lib/03_basics.rb
@@ -0,0 +1,23 @@
+def who_is_bigger(a, b, c)
+	if a == nil || b == nil || c == nil
+		return "nil detected"
+	elsif [a, b, c].max == a
+		return "a is bigger"
+	elsif [a, b, c].max == b
+		return "b is bigger"
+	elsif [a, b, c].max == c
+		return "c is bigger"
+	end
+end
+
+def reverse_upcase_noLTA(string)
+	return string.upcase.reverse.delete "L" "T" "A"
+end
+
+def array_42(arr)
+	return arr.include? 42
+end
+
+def magic_array(arr)
+	return arr.flatten.map{|x| x*2}.select{|x| x%3 !=0}.uniq.sort
+end
